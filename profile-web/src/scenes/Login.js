79da5b9495877b3f1def66b0d9c91aec298f3a98
Josue Valencia <josue.valencia@domo.ec>
2019-06-15 14:51:47 +0200

    theming forms

--- profile-web/src/scenes/Login.js
+++ profile-web/src/scenes/Login.js
@@ -1,7 +1,27 @@
 import React from 'react'
 
-const LoginPage = () => (
-  <h1>Holi</h1>
-)
+class LoginPage extends React.Component {
+  render(){
+    return (
+     <form className="container">
+      <div className="form-group">
+        <label>Email address</label>
+        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+      </div>
+      <div className="form-check">
+        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+        <label className="form-check-label">Check me out</label>
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+    )
+  }
+}
+
 
 export default LoginPage
--- profile-web/src/scenes/Sign.js
+++ profile-web/src/scenes/Sign.js
@@ -1,7 +1,34 @@
 import React from 'react'
 
-const SignPage = () => (
-  <h1>Adios</h1>
-)
+class SignPage extends React.Component {
+  render(){
+    return (
+     <form className="container">
+      <div className="form-group">
+        <label>Email address</label>
+        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+      </div>
+      <div className="form-group">
+        <label>Campus</label>
+        <input type="text" className="form-control" id="exampleInputCampus" aria-describedby="campusHelp" placeholder="Enter Campus" />
+      </div>
+      <div className="form-group">
+        <label>Course</label>
+        <input type="text" className="form-control" id="exampleInputCourse" placeholder="Course" />
+      </div>
+      <div className="form-check">
+        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+        <label className="form-check-label">Check me out</label>
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+    </form>
+    )
+  }
+}
 
 export default SignPage
